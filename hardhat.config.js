538c25d258cac5a3f0d2c5885102e0f2ae876121
tashun <tashun.chamara@gmail.com>
2022-09-11 21:39:34 +0530

    setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,68 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      gas: 2500000,
+      gasPrice: 8000000000
+    },
+    // matic: {
+    //   url: "https://rpc-mumbai.maticvigil.com",
+    //   accounts: [process.env.PRIVATE_KEY],
+    //   gas: 2100000
+    // },
+    // mumbai: {
+    //   url: process.env.STAGING_ALCHEMY_KEY,
+    //   accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    //   gas: 2100000,
+    //   gasPrice: 8000000000
+    // },
+    // rinkeby: {
+    //   url: process.env.STAGING_ALCHEMY_KEY,
+    //   accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    //   gas: 2100000,
+    //   gasPrice: 8000000000
+    // },
+
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.3",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        }
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        }
+      },
+      {
+        version: "0.8.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        }
+      }
+    ]
+  },
 };
